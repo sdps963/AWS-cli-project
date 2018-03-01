68f4e50ac9a355d8b40fe279e755288b5f573e55
Durga Prasad <sdps963@gmail.com>
2018-02-28 21:54:42 -0600

    df

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -17,6 +17,10 @@ def filter_instances(project):
 		
 	return instances
 	
+def has_pending_snapshot(volume):
+	snapshots=list(volume.snapshots.all())
+	return snapshots and snapshots[0].state =='pending'
+	
 @click.group()
 def cli():
 	"""Shotty manages snapshots"""
@@ -78,6 +82,10 @@ def create_snapshots(project):
 		i.wait_until_stopped()
 		
 		for v in i.volumes.all():
+			if has_pending_snapshot(v):
+				print("skipping {0}, snapshot already in progress".format(v.id))
+				continue
+				
 			print("Creating snapshot of {0}".format(v.id))
 			v.create_snapshot(Description="Created by SnapshotsAlyze 30000")
 		
